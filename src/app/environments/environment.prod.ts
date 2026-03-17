1cac15a3f24feaee5f04e3bad1f420b28434486c
nyc360app <nyc360.app@gmail.com>
2026-03-18 02:51:49 +0500

    FIxing

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://globaltruthwatch.runasp.net/api',
-  apiBaseUrl2: 'https://nyc360.runasp.net' 
+  apiBaseUrl2: 'https://nyc360-app.runasp.net' 
 
 };
